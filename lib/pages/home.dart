e13922b212d64b6db6873216eb60611798a058a2
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-10-09 01:33:29 +0330

    add leadbutton to app bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,11 +13,38 @@ class HomePage extends StatelessWidget {
             color: Colors.black,
             fontSize: 18,
           ),
-          
         ),
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0.0,
+        leading: Container(
+          // Change this to a svg icon
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 124, 124, 124),
+              borderRadius: BorderRadius.circular(10)),
+          child: Text(
+            "<",
+            style: TextStyle(fontSize: 18),
+            ),
+        ),
+      actions: [
+        Container(
+          // Change this to a svg icon
+          margin: EdgeInsets.all(10),
+          width: 36,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 124, 124, 124),
+              borderRadius: BorderRadius.circular(10)),
+          child: Text(
+            ":",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+        ),
+
+      ],
       ),
     );
   }
