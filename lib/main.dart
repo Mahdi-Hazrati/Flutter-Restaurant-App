b7adb35ddc1c0efda5d91c24c7506b39970280ec
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-10-09 18:20:29 +0330

    create intro page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_restaurant_app/pages/home_page.dart';
+import 'package:flutter_restaurant_app/pages/intro_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'vazir'),
-      home: const home_page()
+      home: intro_page()
     );
 
   }
